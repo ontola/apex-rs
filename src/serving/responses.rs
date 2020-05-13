c23ad3bc7195e0841ac25732ca8b60695a4913c3
Fletcher91 <thom@argu.co>
2020-05-13 14:27:58 +0200

    Add CORS headers

--- src/serving/responses.rs
+++ src/serving/responses.rs
@@ -6,6 +6,10 @@ pub(crate) fn set_default_headers<'a>(
     res: &'a mut HttpResponseBuilder,
     response_type: &'a ResponseType,
 ) -> &'a mut HttpResponseBuilder {
-    res.set_header(header::CONTENT_TYPE, response_type.to_string())
+    res.set_header(header::SERVER, "Apex/1")
+        .set_header(header::CONTENT_TYPE, response_type.to_string())
+        .set_header(header::ACCESS_CONTROL_ALLOW_ORIGIN, "*")
+        .set_header(header::ACCESS_CONTROL_ALLOW_METHODS, "POST, GET, OPTIONS")
+        .set_header(header::ACCESS_CONTROL_MAX_AGE, 86400u32.to_string())
         .set_header(header::VARY, "Accept, Accept-Encoding, Origin")
 }
