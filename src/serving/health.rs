ef78908fa4f2374732afa7e6139ded461667a83a
Fletcher91 <thom@argu.co>
2020-09-29 17:55:20 +0200

    core#10 Display database name in health endpoint

--- src/serving/health.rs
+++ src/serving/health.rs
@@ -1,3 +1,4 @@
+use crate::app_config::AppConfig;
 use crate::db::db_context::{DbContext, DbPool};
 use crate::serving::response_type::ResponseType::JSON;
 use crate::serving::responses::set_default_headers;
@@ -8,6 +9,7 @@ use serde::Serialize;
 #[derive(Serialize)]
 struct Status {
     name: String,
+    database_name: Option<String>,
     est_documents: i64,
 }
 
@@ -17,9 +19,16 @@ pub(crate) async fn health<'a>(pool: web::Data<DbPool>) -> impl Responder {
     let name = basic_ua();
 
     let counts = ctx.est_counts();
+    let db = AppConfig::default();
+    let database_name = if cfg!(debug_assertions) {
+        Some(db.database_name)
+    } else {
+        None
+    };
 
     let status = Status {
         name,
+        database_name,
         est_documents: counts.documents,
     };
 
